cebb3304f3fd35c3a54ffb426201308bf843e4d4
Yash Jain <93083134+yashj24@users.noreply.github.com>
2023-05-29 13:31:33 +0530

    Add files via upload

---
+++ MovieClient/movie-gold-v1/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter,Rou
+,Route,Routes } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/*" element={<App />}/>
+    </Routes>
+    
+    </BrowserRouter>
+    
+  </React.StrictMode>
+);
+
